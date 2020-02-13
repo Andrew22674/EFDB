e8a54379a494900455a99865fada52ad63127f83
Andrew22674 <avela@unitec.edu>
2020-02-13 17:36:05 -0600

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace EF_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvCars = new System.Windows.Forms.DataGridView();
+            this.CarID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Brand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marca";
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(208, 62);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(175, 22);
+            this.txtBrand.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(112, 314);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvCars
+            // 
+            this.dgvCars.AllowUserToDeleteRows = false;
+            this.dgvCars.BackgroundColor = System.Drawing.Color.OldLace;
+            this.dgvCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCars.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CarID,
+            this.Brand,
+            this.Modelo,
+            this.Year});
+            this.dgvCars.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dgvCars.Location = new System.Drawing.Point(513, 16);
+            this.dgvCars.Name = "dgvCars";
+            this.dgvCars.ReadOnly = true;
+            this.dgvCars.RowHeadersWidth = 51;
+            this.dgvCars.RowTemplate.Height = 30;
+            this.dgvCars.Size = new System.Drawing.Size(566, 422);
+            this.dgvCars.TabIndex = 3;
+            this.dgvCars.DoubleClick += new System.EventHandler(this.dgvCars_DoubleClick);
+            // 
+            // CarID
+            // 
+            this.CarID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.CarID.DataPropertyName = "CarID";
+            this.CarID.HeaderText = "CarID";
+            this.CarID.MinimumWidth = 6;
+            this.CarID.Name = "CarID";
+            this.CarID.ReadOnly = true;
+            this.CarID.Visible = false;
+            // 
+            // Brand
+            // 
+            this.Brand.DataPropertyName = "Brand";
+            this.Brand.HeaderText = "Brand";
+            this.Brand.MinimumWidth = 6;
+            this.Brand.Name = "Brand";
+            this.Brand.ReadOnly = true;
+            this.Brand.Width = 125;
+            // 
+            // Modelo
+            // 
+            this.Modelo.DataPropertyName = "Model";
+            this.Modelo.HeaderText = "Model";
+            this.Modelo.MinimumWidth = 6;
+            this.Modelo.Name = "Modelo";
+            this.Modelo.ReadOnly = true;
+            this.Modelo.Width = 125;
+            // 
+            // Year
+            // 
+            this.Year.DataPropertyName = "Year";
+            this.Year.HeaderText = "Year";
+            this.Year.MinimumWidth = 6;
+            this.Year.Name = "Year";
+            this.Year.ReadOnly = true;
+            this.Year.Width = 125;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Modelo";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(208, 143);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(175, 22);
+            this.txtModel.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Año";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(208, 218);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(175, 22);
+            this.txtYear.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(203, 314);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(297, 314);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1091, 450);
+            this.Controls.Add(this.dgvCars);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvCars;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CarID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Brand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Year;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EF_CRUD
+{
+    public partial class Form1 : Form
+    {
+        Car model = new Car();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearBoxes();
+        }
+
+        void ClearBoxes()
+        {
+            txtBrand.Text = txtModel.Text = txtYear.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.CarID = 0;
+        }
+        //lo que se ejecuta al abrir la ventana
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClearBoxes();
+            FillDGV();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            model.Brand = txtBrand.Text.Trim();
+            model.Model = txtModel.Text.Trim();
+            model.Year = Convert.ToInt32(txtYear.Text.Trim());
+
+            using (DBEntities db = new DBEntities())
+            {
+                if (model.CarID == 0)//Insert
+                    db.Cars.Add(model);
+                else //update
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            ClearBoxes();
+            FillDGV();
+            MessageBox.Show("Saved!");
+        }
+
+        //llenar la data grid view con los registros de la DB
+        void FillDGV()
+        {
+            dgvCars.AutoGenerateColumns = false;
+            using (DBEntities db = new DBEntities())
+            {
+                dgvCars.DataSource = db.Cars.ToList<Car>();
+            }
+        }
+
+        private void dgvCars_DoubleClick(object sender, EventArgs e)
+        {
+            //si la fila seleccionada no es la de los headers (para saber si es una fila valida)
+            if(dgvCars.CurrentRow.Index != -1)
+            {
+                model.CarID = Convert.ToInt32(dgvCars.CurrentRow.Cells["CarID"].Value);
+                using (DBEntities db = new DBEntities())
+                {
+
+                    model = db.Cars.Where(x => x.CarID == model.CarID).FirstOrDefault();
+                    txtBrand.Text = model.Brand;
+                    txtModel.Text = model.Model;
+                    txtYear.Text = model.Year.ToString();
+                    btnSave.Text = "Update";
+                    btnDelete.Enabled = true;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Delete?", "CRUD",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using(DBEntities db = new DBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if(entry.State == EntityState.Detached)
+                    {
+                        db.Cars.Attach(model);
+                    }
+                    db.Cars.Remove(model);
+                    db.SaveChanges();
+                    FillDGV();
+                    ClearBoxes();
+                    MessageBox.Show("Deleted");
+                }
+            }
+        }
+    }
+}
